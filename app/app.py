2702507cd829e157a8bac452b6a5410b6b2d706f
karandeven <karandeven@users.noreply.github.com>
2026-02-19 00:35:10 +0530

    final: omniops devops project completed

--- app/app.py
+++ app/app.py
@@ -1,13 +1,23 @@
-import time
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
 
-APP_NAME = os.getenv("APP_NAME", "OMNIOPS")
-ENV = os.getenv("ENV", "dev")
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
 
-print(f"🚀 {APP_NAME} started")
-print(f"🌍 Environment: {ENV}")
+        app_name = os.getenv("APP_NAME", "OMNIOPS")
+        env = os.getenv("ENV", "dev")
 
-while True:
-    print(f"🙏 Ganpati Bappa Morya | {APP_NAME} alive in {ENV}")
-    time.sleep(10)
+        message = f"""
+        <h1>🚀 {app_name} running in Kubernetes</h1>
+        <p>Environment: {env}</p>
+        """
+        self.wfile.write(message.encode())
+
+PORT = 80
+server = HTTPServer(("0.0.0.0", PORT), Handler)
+print(f"Server running on port {PORT}", flush=True)
+server.serve_forever()
 
